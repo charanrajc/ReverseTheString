4984428b3cec5c0acfd595c1a59e22343297a59e
cherry <cherry@DESKTOP-L3A23Q8>
2018-02-12 09:11:25 +0530

    adding the comment

--- src/ReverseTheString.java
+++ src/ReverseTheString.java
@@ -3,6 +3,7 @@ public class ReverseTheString
 {
 	public static void main(String[] args)
 	{
+		// Reverse String
 		String str = "my java";
 		StringBuffer sb = new StringBuffer(str);
 		System.out.println(" Reverse String :"+sb.reverse());
