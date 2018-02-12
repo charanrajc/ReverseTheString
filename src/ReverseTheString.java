d7c99bfeadfe7999a72393373f563d12f189236f
cherry <cherry@DESKTOP-L3A23Q8>
2018-02-12 09:06:26 +0530

    Intiaily check in the project

---
+++ src/ReverseTheString.java
@@ -0,0 +1,10 @@
+
+public class ReverseTheString 
+{
+	public static void main(String[] args)
+	{
+		String str = "my java";
+		StringBuffer sb = new StringBuffer(str);
+		System.out.println(" Reverse String :"+sb.reverse());
+	} 
+}
